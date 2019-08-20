a621219a188865574d2af8245249681f83737421
JC Spark <sparkdoaz@gmail.com>
2019-08-20 11:23:12 +0800

    done2

--- delegate/delegate/SelectionView.swift
+++ delegate/delegate/SelectionView.swift
@@ -7,3 +7,196 @@
 //
 
 import Foundation
+import UIKit
+
+
+class SelectionView: UIView  {
+    
+    
+    weak var delegate: SelectionViewDelagate? {
+        didSet {
+            
+        }
+    }
+    
+    weak var dataSource: SelectionViewDataSource? {
+        didSet {
+            reload()
+            setIndicatorView()
+            setColorView()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var colors = [UIColor.red, UIColor.yellow, UIColor.blue]
+    
+    var selectedIndex: Int?
+    
+    func reload() {
+        
+        guard let dataSource = dataSource else { return }
+        
+        guard let delegate = delegate else {return}
+        
+        let numberOfButton = dataSource.numberOfButton(in: self)
+        
+        for index in 0 ..< dataSource.numberOfButton(in: self){
+            
+            let buttonWidth = self.frame.width / CGFloat(numberOfButton)
+            let buttonHeight = self.frame.height
+            
+            let button = UIButton(frame: CGRect(
+                x: buttonWidth * CGFloat(index),
+                y: 0,
+                width: buttonWidth,
+                height: buttonHeight))
+            
+            button.backgroundColor = .black
+
+            
+            button.setTitle(
+                dataSource.textOfEachButton(self, index: index),
+                for: .normal)
+            
+            button.titleLabel?.font = dataSource.fontOfEachButton(self)
+            
+            button.setTitleColor(
+                dataSource.colorOfEachText(self),
+                for: .normal)
+            
+            button.tag = index
+            
+            
+                
+            button.addTarget(self, action: #selector(clickedBtn(sender:)), for: .touchUpInside)
+                
+            
+        
+            delegate.didSelected(self, index: index)
+
+            self.addSubview(button)
+        }
+    }
+    
+    
+    var indicatorView = UIView()
+    
+    var colorView = UIView()
+
+    func setIndicatorView() {
+        
+        guard let dataSource = dataSource else { return }
+        
+        indicatorView.backgroundColor = dataSource.indicatorViewColor(self)
+        
+        let numberOfButton = dataSource.numberOfButton(in: self)
+        let buttonWidth = self.frame.width / CGFloat(numberOfButton)
+        
+        //height 改了 y 也要改
+        indicatorView.frame = CGRect(x: 0, y: self.frame.height - 5 , width: buttonWidth, height: 5)
+        self.addSubview(indicatorView)
+    }
+    
+    func setColorView() {
+        
+        guard let dataSource = dataSource else { return }
+        
+        let numberOfButton = dataSource.numberOfButton(in: self)
+        let buttonWidth = self.frame.width / CGFloat(numberOfButton)
+        
+        colorView.backgroundColor = .red
+        colorView.frame = CGRect(x: 0, y: self.frame.height , width: self.frame.width, height: 50)
+        
+        self.addSubview(colorView)
+    }
+    
+    @objc func clickedBtn( sender: UIButton) {
+        
+        selectedIndex = sender.tag
+        
+        print("selectedIndex: \(selectedIndex)")
+        
+        guard let delegate = delegate else { return}
+        
+        if delegate.isCanChoose(self, index: sender.tag) {
+            UIView.animate(withDuration: 0.2) {
+                self.indicatorView.center.x = sender.center.x
+            }
+            
+            self.colorView.backgroundColor = colors[sender.tag % 3]
+            
+            //tableview cell 是一個 view 但是雖然不是 button 但是可以用 ui手勢，只要符合手勢就會觸發下面類似的func，就可以做到 didselected 的效果
+            delegate.didSelected(self, index: sender.tag)
+            
+            
+            
+            print("is:  \(delegate.isCanChoose(self, index: sender.tag))")
+        } 
+        
+        
+    }
+    
+    
+}
+
+
+protocol SelectionViewDelagate: AnyObject {
+    
+    func didSelected(_ selectionView: SelectionView, index: Int) 
+    
+    func isCanChoose(_ selectionView: SelectionView, index: Int) -> Bool
+}
+
+extension SelectionViewDelagate{
+//
+//    func didSelected(_ selectionView: SelectionView, index: Int) -> Int {
+//        return 0
+//    }
+//
+//
+//    func isCanChoose(_ selectionView: SelectionView, index: Int) -> Bool {
+//        return true
+//    }
+}
+
+protocol SelectionViewDataSource: AnyObject  {
+    
+    func numberOfButton(in selectionView: SelectionView) -> Int
+    
+    func textOfEachButton(_ selectionView: SelectionView, index: Int ) -> String
+
+    func indicatorViewColor(_ selectionView: SelectionView) -> UIColor
+
+    func fontOfEachButton(_ selectionView: SelectionView) -> UIFont
+
+    func colorOfEachText(_ selectionView: SelectionView) -> UIColor
+
+}
+//預設
+extension SelectionViewDataSource {
+    
+    func numberOfButton(in selectionView: SelectionView) -> Int {
+        return 2
+    }
+    
+    func colorOfEachText(_ selectionView: SelectionView) -> UIColor {
+        return .white
+    }
+    
+    func fontOfEachButton(_ selectionView: SelectionView) -> UIFont {
+        return .systemFont(ofSize: 18)
+    }
+    
+    func indicatorViewColor(_ selectionView: SelectionView) -> UIColor {
+        return .blue
+    }
+    
+}
+
--- delegate/delegate/ViewController.swift
+++ delegate/delegate/ViewController.swift
@@ -8,13 +8,114 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate {
+    
+    var selectiobView: SelectionView! {
+        didSet {
+            selectiobView.dataSource = self
+            selectiobView.delegate = self
+        }
+    }
+    
+    var selectionViewTwo: SelectionView! {
+        didSet {
+            selectionViewTwo.dataSource = self
+            selectionViewTwo.delegate = self
+        }
+    }
+    
+    var fake = ["Julia", "呂世軒", "熊仔", "街巷", "LeoWang"]
+    var fakeTwo = ["kkbox", "spotify", "AppleMusic"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //safearea
+        self.selectiobView = SelectionView(frame: CGRect(x: 0, y: 40, width: UIScreen.main.bounds.width, height: 100))
+        selectionViewTwo = SelectionView(frame: CGRect(x: 0, y: 200, width: UIScreen.main.bounds.width, height: 100))
+        self.view.addSubview(selectiobView)
+        self.view.addSubview(selectionViewTwo)
+        self.selectionViewTwo.delegate = self
+        self.selectiobView.delegate = self
+        self.selectionViewTwo.dataSource = self
+        self.selectiobView.dataSource = self
+        //一開始只有寫didset 不會有數字跳出來
+        
+        view.backgroundColor = .black
     }
 
 
+}
+
+extension ViewController: SelectionViewDelagate {
+    func didSelected(_ selectionView: SelectionView, index: Int) {
+        print(index)
+//        switch selectionView {
+//        case selectiobView:
+//            <#code#>
+//        default:
+//
+//        }
+    
+    }
+    
+    func isCanChoose(_ selectionView: SelectionView, index: Int) -> Bool {
+        switch selectionView {
+        case selectiobView:
+            return true
+            
+        case selectionViewTwo:
+            
+            if selectiobView.selectedIndex == fake.count - 1 {
+                
+                return false
+            }
+            return true
+        default:
+            return true
+        }
+    }
+}
+
+extension ViewController: SelectionViewDataSource {
+    
+    
+    func colorOfEachText(_ selectionView: SelectionView) -> UIColor {
+        switch selectionView {
+        case self.selectiobView:
+            return .white
+        case  self.selectionViewTwo:
+            return .white
+        default:
+            return .brown
+        }
+    }
+    
+    func textOfEachButton(_ selectionView: SelectionView, index: Int) -> String {
+        switch selectionView {
+        case selectiobView:
+            return fake[index]
+        case selectionViewTwo:
+            return fakeTwo[index]
+        default:
+            return ""
+        }
+    }
+    
+    func numberOfButton(in selectionView: SelectionView) -> Int {
+        switch selectionView {
+        case selectiobView:
+            return fake.count
+        case selectionViewTwo:
+            return fakeTwo.count
+        default:
+            return 2
+        }
+    }
+    
+    
+    
+    
 }
 
