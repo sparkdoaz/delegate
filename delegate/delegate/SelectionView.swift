23ee4a5412a510438fe4ec5c8d2d2db30801d1d3
JC Spark <sparkdoaz@gmail.com>
2019-08-20 11:17:29 +0800

    done

---
+++ delegate/delegate/SelectionView.swift
@@ -0,0 +1,9 @@
+//
+//  SelectionView.swift
+//  delegate
+//
+//  Created by 黃建程 on 2019/8/19.
+//  Copyright © 2019 Spark. All rights reserved.
+//
+
+import Foundation
---
+++ delegate/delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegate
+//
+//  Created by 黃建程 on 2019/8/19.
+//  Copyright © 2019 Spark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
